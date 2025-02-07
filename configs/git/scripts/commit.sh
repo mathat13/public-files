d45ae182f77bc354f2129ba7426422fbbdcee676
mathat13 <lovell.jwz@gmail.com>
2025-02-07 17:40:38 +0000

    daily crontab backup Fri Feb  7 05:40:38 PM UTC 2025

--- configs/git/scripts/commit.sh
+++ configs/git/scripts/commit.sh
@@ -1,18 +1,20 @@
+#!/bin/bash
+
 dirs=(
     "/shares/dfsroot/synced_files"
     "/shares/dfsroot/codeshare"
 )
 
 # State time and date
-echo -e "\e[93mDaily crontab backup for `date`\n\e[0m"
+echo -e "\e[93mDaily crontab backup for `date`:\n\e[0m"
 
 for dir in "${dirs[@]}"; do
 
-    # State which directory we're pushing to
-    echo -e "\e[32m- Pushing contents of $dir to GitHub repository:\n\e[0m"
-
     # cd to public directory
     cd $dir || exit 1
+
+    # State which directory we're pushing to
+    echo -e "\e[32m- Pushing contents of $dir to GitHub repository:\n\e[0m"
 
     # Add and commit changes
     /usr/bin/git add --all
