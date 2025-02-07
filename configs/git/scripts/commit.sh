2c43c0478680006d12cbca6c4f471d3984ae3435
mathat13 <lovell.jwz@gmail.com>
2025-02-07 18:03:32 +0000

    daily crontab backup Fri Feb  7 06:03:32 PM UTC 2025

--- configs/git/scripts/commit.sh
+++ configs/git/scripts/commit.sh
@@ -12,10 +12,10 @@ echo -e "\e[93mDaily crontab backup for `date`:\n\e[0m"
 for dir in "${dirs[@]}"; do
 
     # cd to public directory and return error and exit script if dir not found
-    cd $dir 2>/dev/null || { echo -e "\e[31m[Error] Directory '$dir' not found! \e[0m"; exit 1;}
+    cd $dir 2>/dev/null || { echo -e "\e[31m[Error] Directory '$dir' not found! \n\e[0m"; exit 1;}
 
     # State which directory we're pushing to
-    echo -e "\e[93m- Pushing contents of $dir to GitHub repository:\n\e[0m"
+    echo -e "\e[93m[Status] Pushing contents of $dir to GitHub repository:\n\e[0m"
 
     # Add and commit changes
     /usr/bin/git add --all
