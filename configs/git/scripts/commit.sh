49eba8608e8b1b77fa7a8786a6dfa49fca944c19
mathat13 <lovell.jwz@gmail.com>
2025-02-07 17:58:12 +0000

    daily crontab backup Fri Feb  7 05:58:12 PM UTC 2025

--- configs/git/scripts/commit.sh
+++ configs/git/scripts/commit.sh
@@ -3,6 +3,7 @@
 dirs=(
     "/shares/dfsroot/synced_files"
     "/shares/dfsroot/codeshare"
+    "/poop"
 )
 
 # State time and date
@@ -10,11 +11,11 @@ echo -e "\e[93mDaily crontab backup for `date`:\n\e[0m"
 
 for dir in "${dirs[@]}"; do
 
-    # cd to public directory
-    cd $dir || exit 1
+    # cd to public directory and return error and exit script if dir not found
+    cd $dir 2>/dev/null || { echo -e "\e[31m[Error] Directory '$dir' not found! \e[0m"; exit 1;}
 
     # State which directory we're pushing to
-    echo -e "\e[32m- Pushing contents of $dir to GitHub repository:\n\e[0m"
+    echo -e "\e[93m- Pushing contents of $dir to GitHub repository:\n\e[0m"
 
     # Add and commit changes
     /usr/bin/git add --all
@@ -23,5 +24,5 @@ for dir in "${dirs[@]}"; do
     # send data to Git server
     /usr/bin/git push origin main
 
-    echo -e "\e[32m\n- Push completed.\n\e[0m"
+    echo -e "\e[32m\n[Success] Push completed.\n\e[0m"
 done
