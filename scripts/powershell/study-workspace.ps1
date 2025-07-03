29501e885c371faee3722b908ef3c04df2256115
mathat13 <lovell.jwz@gmail.com>
2025-07-03 04:00:01 +0000

    daily crontab backup Thu Jul  3 04:00:01 AM UTC 2025

--- scripts/powershell/study-workspace.ps1
+++ scripts/powershell/study-workspace.ps1
@@ -1,22 +1,49 @@
+# HELPER FUNCTIONS
+
+function Get-ProgramRegistryKey {
+    param (
+        # Default value assigned to common locations to find registry keys with required executable locations
+        [string[]] $RegistryPath = @(
+                        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+                        'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall',
+                        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\',
+                        'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+                        ),
+        # Can provide blank value to show all registry keys at given RegistryPath, with properties
+        [string] $Name
+    )
+
+    foreach ($path in $RegistryPath) {
+        if (test-path $path) {
+            $regkeys = Get-ChildItem $path
+            # Extract all properties of registry keys that contain the Name and store as registry key
+            $regkeys | Where-Object {
+                $values = (Get-ItemProperty $_.PSPath)
+                $values.DisplayName -like "*$Name*" -or $values.'(default)' -like "*$Name*" }
+        }
+        else {Write-Host "Path: $path not found, skipping."}
+    }
+}
+
+function Get-RegistryValue {
+    # $(Get-ProgramRegistryKey -Name "Microsoft Visual Studio Code").Name gives full registry path, could possibly pass this here and link up both functions
+    param (
+        [String] $KeyString,
+        [string] $Value
+        )
+
+        $registrykey = Get-Item $KeyString
+        $(Get-ItemProperty $registrykey.PSPath).$Value
+}
 
 # FIREFOX
-$firefoxpath = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\firefox.exe\").'(default)'
-
+$firefoxpath = Get-RegistryValue -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\firefox.exe" -Value '(default)'
 start-process $firefoxpath
 
 # VSCODE
 # Get registry paths for vscode installations
-$vscode = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall' |
-    ForEach-Object { Get-ItemProperty $_.PSPath } |
-    Where-Object { $_.DisplayName -like 'Microsoft Visual Studio Code' }
-
-# Executes if only one installation present on machine, skips otherwise.
-if ( $vscode -isnot [Array] ) {
-    start-process $vscode.displayicon
-}
-else {
-    write-host "Multiple VSCode installations present on machine, skipping."
-}
+$vscodepath = Get-RegistryValue -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{EA457B21-F73E-494C-ACAB-524FDE069978}_is1" -Value 'DisplayIcon'
+start-process $vscodepath
 
 # WSL
 # works
@@ -39,5 +66,5 @@ else {
 # Maybe add key at install
 Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall' |
     ForEach-Object { Get-ItemProperty $_.PSPath } |
-    Where-Object { $_.DisplayName -like '*cherrytree*' } |
-    Select-Object DisplayName, InstallLocation, DisplayIcon
+    Where-Object { $_.Name -like '*cherrytree*' } |
+    Select-Object Name, InstallLocation, DisplayIcon
